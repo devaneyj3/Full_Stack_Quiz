8904d0e0b94a89554c818b000aff496c5c83cf34
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-23 13:06:09 -0400

    create react app and nagivation component

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Nav.scss';
+
+const Nav = () => {
+   return(
+       <section className='Nav'>
+           <nav>
+               <NavLink to='/teachers'>Teachers</NavLink> 
+               <NavLink to='/students'>Students</NavLink>
+               <NavLink to='/quizes'>Quizes</NavLink>
+           </nav>
+    </section>
+  )
+}
+
+export default Nav;
---
+++ client/src/container/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Nav from '../components/Nav/Nav';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Nav/>
+       teachers
+      students
+      quizes
+    </div>
+  );
+}
+
+export default App;
