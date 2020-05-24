8c17400d541bcc74539211f99c03cffe7b6f7011
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-24 00:22:45 -0400

    add some styling to teacher page

--- client/src/components/Teacher/Teacher.js
+++ client/src/components/Teacher/Teacher.js
@@ -6,6 +6,9 @@ const Teacher = (props) => {
       <h1>{props.name}</h1>
       <h1>{props.email}</h1>
       <h1>{props.class}</h1>
+      {/* TODO:use reactstrap buttons */}
+      <button>Edit</button>
+      <button>Delete</button>
     </section>
   );
 };
--- client/src/components/Teachers/Teachers.js
+++ client/src/components/Teachers/Teachers.js
@@ -7,19 +7,21 @@ const Teachers = () => {
   const data = useContext(teacherContext);
 
   return (
-    <section className="Teachers">
+    <>
       <h1>Here is a list of all the teachers</h1>
-      {data.data.map((teacher) => {
-        return (
-          <Teacher
-            key={teacher.id}
-            name={teacher.name}
-            email={teacher.email}
-            class={teacher.class}
-          />
-        );
-      })}
-    </section>
+      <section className="Teachers">
+        {data.data.map((teacher) => {
+          return (
+            <Teacher
+              key={teacher.id}
+              name={teacher.name}
+              email={teacher.email}
+              class={teacher.class}
+            />
+          );
+        })}
+      </section>
+    </>
   );
 };
 
